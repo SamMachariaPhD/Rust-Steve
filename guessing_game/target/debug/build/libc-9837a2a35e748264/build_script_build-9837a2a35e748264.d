00ab6b07304bb0a952ffa64254cd95c8443ded2e
sirmaxford <sirmaxford@gmail.com>
2021-07-10 23:01:43 +0900

    Semantic versioining

---
+++ guessing_game/target/debug/build/libc-9837a2a35e748264/build_script_build-9837a2a35e748264.d
@@ -0,0 +1,5 @@
+/home/sam/Documents/RUST/forGit/Rust-Steve/guessing_game/target/debug/build/libc-9837a2a35e748264/build_script_build-9837a2a35e748264: /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.98/build.rs
+
+/home/sam/Documents/RUST/forGit/Rust-Steve/guessing_game/target/debug/build/libc-9837a2a35e748264/build_script_build-9837a2a35e748264.d: /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.98/build.rs
+
+/home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.98/build.rs:
---
+++ guessing_game/target/debug/deps/rand-0125f2757457460f.d
@@ -0,0 +1,9 @@
+/home/sam/Documents/RUST/forGit/Rust-Steve/guessing_game/target/debug/deps/rand-0125f2757457460f.rmeta: /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/sam/Documents/RUST/forGit/Rust-Steve/guessing_game/target/debug/deps/librand-0125f2757457460f.rlib: /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/sam/Documents/RUST/forGit/Rust-Steve/guessing_game/target/debug/deps/rand-0125f2757457460f.d: /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
+/home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
+/home/sam/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
